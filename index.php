6269e9483a7dcabb8b86a67c8af14ed3669968a6
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-09-15 16:46:28 -0400

    Updated index page with HTML Tags for title, charset and headings needed

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Project 1 for CSCI E-15</title>
+	</head>
 	<body>
+		<h1>DWA Project: P1</h1>
+		<h2>Workflow setup and essential HTML/CSS demonstration</h2>
+		<div>
+			<h3>My Name :</h3>
+			<h3>About me :</h3>
+			<h3>Code Editor used :</h3>
+		</div>
 		<?php
-			echo "Welcome to P1";
+			// PHP code goes here;
 		?>
 	</body>
 </html>
