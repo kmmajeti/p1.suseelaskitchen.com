ab7eb4842bb247f5e56b35bb323f1a811dfe48b6
Krishna Murthy Majeti <kmmajeti@hotmail.com>
2013-09-15 17:15:13 -0400

    Updated index page with information like my name, small bio of myself and the name of the code editor using

--- index.php
+++ index.php
@@ -9,8 +9,15 @@
 		<h2>Workflow setup and essential HTML/CSS demonstration</h2>
 		<div>
 			<h3>My Name :</h3>
+			<p>KRISHNA MURTHY MAJETI</p>
 			<h3>About me :</h3>
-			<h3>Code Editor used :</h3>
+			<p>I am currently working as a Principal Software Quality Engineer at <a href="http://www.ptc.com/">PTC Inc.</a>
+			and possess more than 15+ years of Software testing (QA) experience in testing web client and server applications.
+			I received a bachelor's degree in Mechanical Engineering from Shivaji University, India. My hobbies are watching 
+			TV, gardening and family time. I have always been a fan of Indian food and therefore plan to use this domain to 
+			develop a web page with delicious recipes of my wife after this course.</p>
+			<h3>Code Editor :</h3>
+			<p>Sublime Text</p>
 		</div>
 		<?php
 			// PHP code goes here;
